b51b6bfb9a865071550fc5c79d2e7abc31e3f42c
sWgiraffe <346844825@qq.com>
2018-03-06 06:16:26 -0800

    picture location

--- src/components/GalleryByReactApp.js
+++ src/components/GalleryByReactApp.js
@@ -22,15 +22,41 @@ imageDatas = (function getImageURL(imageDatasArr) {
     return imageDatasArr;
 })(imageDatas);
 
+var ImgFigure = React.createClass({
+    render: function () {
+        return (
+            <figure className = "img-figure">
+                <img src = {this.props.data.imageURL}
+                     alt = {this.props.data.title}
+                />
+                <figcaption>
+                    <h2 className = "img-title">{this.props.data.title}</h2>
+                </figcaption>
+            </figure>
+        );
+    }
+
+});
+
 
 
 var GalleryByReactApp = React.createClass({
   render: function() {
+
+      var controllerUnits = [],
+          imgFigures = [];
+
+      imageDatas.forEach(function (value) {
+          imgFigures.push(<ImgFigure data = {value}/>);
+      });
+
       return (
         <section className = "stage">
             <section className = "img-sec">
+                {imgFigures}
             </section>
             <nav className = "controller-nav">
+                {controllerUnits}
             </nav>
         </section>
       );
